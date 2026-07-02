f01a9d133e92711aadc26bf947aa169e1958f365
tonybbb2 <tony.bui.1@ens.etsmtl.ca>
2026-07-02 19:07:32 -0400

    idk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -366,7 +366,7 @@ function App() {
                 <div className="mt-5 flex items-end justify-between gap-3">
                   {selectedFiles.length > 0 && (
 
-                      <div className="flex items-center gap-2">
+                      <div className="flex justify-end gap-2">
                         {selectedFiles.map((file, idx) => (
                           <div
                             key={file.name + file.lastModified}
